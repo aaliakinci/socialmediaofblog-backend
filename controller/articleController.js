50ffdca81a00c4a8517f6ba3bfb8ac7c69f5f88e
aaliakinci <aliakiinci@gmail.com>
2021-01-16 01:05:48 +0300

    get articles by user_id lookup user routes created

--- controller/articleController.js
+++ controller/articleController.js
@@ -1,74 +1,159 @@
-
 //Models
+const { json } = require('express');
 const mongoose = require('mongoose');
 const Article = require('../Models/Article');
+const User = require('../Models/User');
 
 //functions
-const {addArticleToUser} = require('./functionArea');
-
+const { addArticleToUser } = require('./functionArea');
 
 //Get All Article
-const getAllArticle = (req,res,next) => {
-const promise = Article.find({})
-promise.then((data)=>{
-	res.json(data);
-}).catch((err)=>{
-	console.log(err);
-})
-}
+const getAllArticle = (req, res, next) => {
+	const promise = Article.find({});
+	promise
+		.then((data) => {
+			res.json(data);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
 
 //Get All Article Sort By ReactionPoint
-const getAllArticleSortByReactionPoint = (req,res,next) => {
-	const promise =Article.aggregate([
+const getAllArticleSortByReactionPoint = (req, res, next) => {
+	const promise = Article.aggregate([
 		{
-			$lookup:{
-				from:'users',
-				localField:'user_id',
-				foreignField:'_id',
-				as:'user'
-			}
+			$lookup: {
+				from: 'users',
+				localField: 'user_id',
+				foreignField: '_id',
+				as: 'user',
+			},
 		},
 		{
-			$unwind:{
-				path:'$user'
-			}
+			$unwind: {
+				path: '$user',
+			},
 		},
 		{
-			$group:{
-				_id:{
-					_id:'$_id',
-					title:'$title',
-					description:'$description',
-					createAt:'$createAt',
+			$group: {
+				_id: {
+					_id: '$_id',
+					title: '$title',
+					description: '$description',
+					createAt: '$createAt',
 				},
-				user:{
-					$push:'$user'
-				}
+				user: {
+					$push: '$user',
+				},
+			},
+		},
+		{
+			$project: {
+				_id: '$_id._id',
+				title: '$_id.title',
+				description: '$_id.description',
+				createAt: '$_id.createAt',
+				user: '$user',
+			},
+		},
+		{
+			$sort: {
+				reactionPoint: -1,
+				createAt: -1,
+			},
+		},
+	]);
+	promise
+		.then((data) => {
+			res.status(200).json(data);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+};
+
+//Get Articles by user_id
+const getArticlesByUser_id = async(req,res,next) => {
+	const id = req.params.user_id;
+ 
+	const promise = Article.aggregate([
+		{
+			$match:{
+				user_id:mongoose.Types.ObjectId(id)
 			}
 		},
 		{
-			$project:{
-				_id:'$_id._id',
-				title:'$_id.title',
-				description:'$_id.description',
-				createAt:'$_id.createAt',
-				user:'$user'
-			}
+			$lookup: {
+				from: 'users',
+				localField: 'user_id',
+				foreignField: '_id',
+				as: 'user',
+			},
 		},
 		{
-			$sort:{
-				reactionPoint:-1,
-				createAt:-1
-			}
-		}
+			$unwind: {
+				path: '$user',
+			},
+		},
+		{
+			$group: {
+				_id: {
+					_id: '$_id',
+					title: '$title',
+					description: '$description',
+					createAt: '$createAt',
+				},
+				user: {
+					$push: '$user',
+				},
+			},
+		},
+		{
+			$project: {
+				_id: '$_id._id',
+				title: '$_id.title',
+				description: '$_id.description',
+				createAt: '$_id.createAt',
+				user: '$user',
+			},
+		},
+		{
+			$sort: {
+				createAt: -1,
+			},
+		},
 	]);
 	promise.then((data)=>{
-		res.status(200).json(data)
+	 res.status(200).json(data);
 	}).catch((err)=>{
-		res.json(err);
+		console.log(err);
 	})
 }
 
+
+
+
+
+
+
+//Get All follows Article sort by lastTime
+const followsArticle = async (req, res, next) => {
+	
+	try {
+		const { user_id } = req.body;
+		const user = await User.findById(user_id);
+	  // const output = user.follows.map((item) => {
+		//  const promise = Article.find({ user_id: item });
+   	// 	 promise.then((data)=>{
+		//  }).catch((err)=>{
+		// 	 console.log(err);
+		//  })
+		// }) 
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 //Create Article
 const createArticle = async (req, res, next) => {
@@ -82,15 +167,17 @@ const createArticle = async (req, res, next) => {
 			hashtags,
 		});
 		const createdArticle = await article.save();
-		const importUser = await addArticleToUser(createdArticle._id,user_id)
-		res.status(200).json({createdArticle})
+		const importUser = await addArticleToUser(createdArticle._id, user_id);
+		res.status(200).json({ createdArticle });
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-module.exports= {
+module.exports = {
 	createArticle,
 	getAllArticle,
-	getAllArticleSortByReactionPoint
-}
+	getAllArticleSortByReactionPoint,
+	followsArticle,
+	getArticlesByUser_id,
+};
--- routes/articles.js
+++ routes/articles.js
@@ -12,9 +12,11 @@ router.get('/',articleController.getAllArticle)
 //Get All Sort By ReactionPoint
 router.get('/reactionPoint',articleController.getAllArticleSortByReactionPoint)
 
+//Get Follows Article 
+router.get('/followsArticle',articleController.followsArticle)
 
-
-
+//Get Articles by User Id 
+router.get('/:user_id',articleController.getArticlesByUser_id)
 
 //Create Article
 router.post('/create',articleController.createArticle);
