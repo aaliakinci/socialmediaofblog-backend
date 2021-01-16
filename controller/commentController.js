1f864e43677f90586cd0adda31d52d2d7e53f7d9
aaliakinci <aliakiinci@gmail.com>
2021-01-16 22:52:45 +0300

    create getCommentsbyArticleid

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,13 +1,15 @@
-
+//Models
 const Comment = require('../Models/Comment');
+
+
 
 //function area
 const {addCommentToUser,addCommentToArticle} = require('./functionArea')
 
-
+//mongoose
+const mongoose = require('mongoose')
 
 const createComment = async(req,res,next) => {
-
 	try {
 		const {user_id,article_id,description} = req.body
 		const comment = new Comment({
@@ -23,7 +25,26 @@ const createComment = async(req,res,next) => {
 		console.log(error);	
 	}
 }
+const getCommentsByArticleId = async(req,res,next) => {
+	try {
+		const id = req.params.article_id
+		const comments = await Comment.find({article_id:id}).sort({createAt:-1});
+		res.status(200).json(comments);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+
+
+
+
+
+
+
+
 
 module.exports = {
 	createComment,
+	getCommentsByArticleId
 }
--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,8 @@ const commentController = require('../controller/commentController')
 //Create comment
 router.post('/create',commentController.createComment);
 
-
+//Get Comments by Article id
+router.get('/:article_id',commentController.getCommentsByArticleId);
 
 
 module.exports=router;
